105c43a9c32f77b70e0391961c3843a8225484ba
khy <kouhanyao@126.com>
2018-07-19 21:15:09 +0800

    自定义返回信息

---
+++ jwt-server/src/main/java/com/khy/jwt/JwtApplication.java
@@ -0,0 +1,16 @@
+package com.khy.jwt;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@MapperScan("com.khy.jwt.dao")
+@EnableTransactionManagement
+public class JwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtApplication.class, args);
+    }
+}
