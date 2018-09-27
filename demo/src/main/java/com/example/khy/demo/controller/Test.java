28c501a9eb7efbd34c5887f29b12b51911521946
khy <kouhanyao@126.com>
2018-09-27 22:40:56 +0800

    自定义返回信息

--- demo/src/main/java/com/example/khy/demo/controller/Test.java
+++ demo/src/main/java/com/example/khy/demo/controller/Test.java
@@ -19,19 +19,19 @@ public class Test {
 
         List<Map<String, Object>> table1 = new ArrayList<Map<String, Object>>();
         Map<String, Object> map1 = new HashMap<>();
-        map1.put("name", "张三");
-        map1.put("age", "23");
-        map1.put("email", "12121@qq.com");
+        map1.put("lenderName", "张三");
+        map1.put("lenderCardNum", "1651351456165413");
+        map1.put("certificateType", "身份证");
 
         Map<String, Object> map2 = new HashMap<>();
-        map2.put("name", "李四");
-        map2.put("age", "45");
-        map2.put("email", "45445@qq.com");
+        map2.put("lenderName", "李四");
+        map2.put("lenderCardNum", "45");
+        map2.put("certificateType", "45445@qq.com");
 
         Map<String, Object> map3 = new HashMap<>();
-        map3.put("name", "Tom");
-        map3.put("age", "34");
-        map3.put("email", "6767@qq.com");
+        map3.put("lenderName", "Tom");
+        map3.put("lenderCardNum", "34");
+        map3.put("certificateType", "6767@qq.com");
 
         table1.add(map1);
         table1.add(map2);
@@ -40,34 +40,104 @@ public class Test {
 
         List<Map<String, Object>> table2 = new ArrayList<Map<String, Object>>();
         Map<String, Object> map4 = new HashMap<>();
-        map4.put("name", "tom");
-        map4.put("number", "sd1234");
-        map4.put("address", "上海");
+        map4.put("lenderName", "tom");
+        map4.put("amount", "sd1234");
+        map4.put("bidMonth", "上海");
+        map4.put("bidRate", "上海");
+        map4.put("start", "上海");
+        map4.put("end", "上海");
+        map4.put("day", "上海");
+        map4.put("total", "上海");
 
         Map<String, Object> map5 = new HashMap<>();
-        map5.put("name", "seven");
-        map5.put("number", "sd15678");
-        map5.put("address", "北京");
+        map5.put("lenderName", "seven");
+        map5.put("amount", "sd15678");
+        map5.put("bidMonth", "北京");
+        map5.put("bidRate", "北京");
+        map5.put("start", "北京");
+        map5.put("end", "北京");
+        map5.put("day", "北京");
+        map5.put("total", "北京");
 
         Map<String, Object> map6 = new HashMap<>();
-        map6.put("name", "lisa");
-        map6.put("number", "sd9078");
-        map6.put("address", "广州");
+        map6.put("lenderName", "lisa");
+        map6.put("amount", "sd9078");
+        map6.put("bidMonth", "广州");
+        map6.put("bidRate", "广州");
+        map6.put("start", "广州");
+        map6.put("end", "广州");
+        map6.put("day", "广州");
+        map6.put("total", "广州");
 
         table2.add(map4);
         table2.add(map5);
         table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
+        table2.add(map6);
 
 
-        parametersMap.put("userName", "JUVENILESS");
-        parametersMap.put("time", "2018-03-24");
-        parametersMap.put("sum", "3");
+        parametersMap.put("contractId", "hd254f2saf4245jla543sf43jl");
+        parametersMap.put("borrowerName", "李强");
+        parametersMap.put("borrowerCardNum", "510256632585420232");
+        parametersMap.put("address", "四川省成都市");
+        parametersMap.put("certificateType", "身份证");
 
 
         wordDataMap.put("table1", table1);
         wordDataMap.put("table2", table2);
         wordDataMap.put("parametersMap", parametersMap);
-        File file = new File("D:\\Workspaces\\Eclipse 2017\\wordTemplate\\doc\\模板.docx");//改成你本地文件所在目录
+        File file = new File("E:\\test\\借款协议.docx");//改成你本地文件所在目录
 
 
         // 读取word模板
@@ -79,10 +149,10 @@ public class Test {
 
 
         //生成文件
-        File outputFile = new File("D:\\Workspaces\\Eclipse 2017\\wordTemplate\\doc\\输出.docx");//改成你本地文件所在目录
+        File outputFile = new File("E:\\test\\输出.docx");//改成你本地文件所在目录
         FileOutputStream fos = new FileOutputStream(outputFile);
         template.getDocument().write(fos);
-
+        System.out.println("success");
     }
 
 }
